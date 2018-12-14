5ef2bb056dca8334329b8d3568aae3381d086e57
nerio2 <Patryknerio2@gmail.com>
2018-12-15 00:56:17 +0100

    update

--- API_src/src/main/java/first/GreetingController.java
+++ API_src/src/main/java/first/GreetingController.java
@@ -4,7 +4,7 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 public class GreetingController {
@@ -12,8 +12,9 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    @CrossOrigin(origins = "*")
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestHeader(value="User-Agent") String userAgent,@RequestParam(value="name", defaultValue="World") String name) {
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
     }
